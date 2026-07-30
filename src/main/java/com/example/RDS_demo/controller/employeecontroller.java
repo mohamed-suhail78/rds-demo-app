4f3e61feb436b34f1c472e1208c4687b048d1f5e
NEW_USERNAME <suhail@biznessforce.com>
2026-07-30 16:42:20 +0530

    EC2 CI/CD Test

--- src/main/java/com/example/RDS_demo/controller/employeecontroller.java
+++ src/main/java/com/example/RDS_demo/controller/employeecontroller.java
@@ -24,7 +24,7 @@ public class employeecontroller {
     	}
     	@GetMapping("/version")
     	public String version() {
-    	    return ("after ci/cd deployment");
+    	    return ("after 	EC2 ci/cd deployment");
     	}
 
  
